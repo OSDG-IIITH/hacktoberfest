78bc007251f5542e1150419c05f5a6442a5bd243
GADILA SHASHANK REDDY <shashank.reddy@research.iiit.ac.in>
2017-10-18 16:04:46 +0530

    makeExec

---
+++ makeExec.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+filenm=$1
+if [ "$#" -ne 1 ]
+then
+	echo "Input a filename (only one) whose permission is to be changed"
+
+elif [ ! -f $filenm ]
+then
+	echo "Not a File"
+
+elif [ -f $filenm ]
+then
+	chmod u+x $filenm
+fi
+	
+
