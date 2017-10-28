5f28eadc69e0df87c6803e1301f26ad8eb709425
aradhya-tongia15 <32356852+aradhya-tongia15@users.noreply.github.com>
2017-10-28 18:11:05 +0000

    Add files via upload
    A code to calculate avg in python

---
+++ avg.py
@@ -0,0 +1,10 @@
+
+round1 = int(raw_input("Enter score for round 1: "))
+
+round2 = int(raw_input("Enter score for round 2: "))
+
+round3 = int(raw_input("Enter score for round 3: "))
+   
+average = (round1 + round2 + round3) / 3
+
+print "the average score is: ", average
